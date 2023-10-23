ca1e9742a0978c9a6b34a200975ea6502eab741a
Femzysamie <femzysamiejebty@gmail.com>
2023-10-23 06:53:37 -0700

    updated

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""
-Uses https://jsonplaceholder.typicode.com along with an employee ID to
-return information about the employee's todo list progress
-"""
+""" a script that converts data to json """
+
 
 import json
 import requests
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    userId = argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
-                        format(userId), verify=False).json()
-    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
-                        format(userId), verify=False).json()
-    username = user.get('username')
-    tasks = []
-    for task in todo:
-        task_dict = {}
-        task_dict["task"] = task.get('title')
-        task_dict["completed"] = task.get('completed')
-        task_dict["username"] = username
-        tasks.append(task_dict)
-    jsonobj = {}
-    jsonobj[userId] = tasks
-    with open("{}.json".format(userId), 'w') as jsonfile:
-        json.dump(jsonobj, jsonfile)
+
+if __name__ == "__main__":
+    userId = sys.argv[1]
+    user = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'
+            .format(userId)
+            )
+
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    todoUser = {}
+    taskList = []
+
+    for task in todos.json():
+        if task.get('userId') == int(userId):
+            taskDict = {
+                    "task": task.get('title'),
+                    "completed": task.get('completed'),
+                    "username": user.json().get('username')
+                    }
+            taskList.append(taskDict)
+        todoUser[userId] = taskList
+
+    filename = userId + '.json'
+
+    with open(filename, mode="w") as f:
+        json.dump(todoUser, f)
